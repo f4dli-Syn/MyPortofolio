5c7a91494acd6303d8d8359c688efe1ea814b853
unknown <ibnufadli12gf@gmail.com>
2024-07-05 04:18:24 +0700

    penambahan fitur

---
+++ script/script.js
@@ -0,0 +1,9 @@
+const teksProject = document.querySelector("#teks-project");
+
+teksBlok.addEventListener("mouseover", function () {
+  teksBlok.style.opacity = 0.5;
+});
+
+teksBlok.addEventListener("mouseout", function () {
+  teksBlok.style.opacity = 1;
+});
